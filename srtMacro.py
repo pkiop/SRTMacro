c9aa0dc64cbe25c9c460d1372c6a3048779be69e
pkiop <pkiop@naver.com>
2020-11-22 09:51:26 +0900

    feat: update 3range

--- srtMacro.py
+++ srtMacro.py
@@ -5,7 +5,9 @@ print("dodo")
 print(pyautogui.size())
 time.sleep(2)
 x, y = pyautogui.position()
-
+print("loca : ", x, y)
+ver = input("put version ( 1 : one pick 2 : 3 pick)")
+offset=16
 while True :
     
     print(x, y)
@@ -13,9 +15,19 @@ while True :
     pyautogui.press("r")
     pyautogui.keyUp("command")
     pyautogui.press("enter")
-    time.sleep(0.6)
-    for i in range(0, 5):
-        pyautogui.click(x=x, y=y+i*4)
-    for i in range(0, 5):
-        pyautogui.click(x=x, y=y+i*4)
+    time.sleep(0.9)
+    if ver == "1":
+        for i in range(0, 5):
+       	    pyautogui.click(x=x, y=y+i*6)
+        for i in range(0, 5):
+            pyautogui.click(x=x, y=y+i*6)
     
+    if ver == "2":
+        for i in range(0,5):
+            pyautogui.click(x=x, y=y+i*4)
+        for i in range(offset+0,offset+5):
+            pyautogui.click(x=x, y=y+i*4)
+        for i in range(offset*2+0,offset*2+5):
+            pyautogui.click(x=x, y=y+i*4)
+
+
